f864c109a76a2ca30b09eadaaf0328fff3a61094
Roman Luštrik <romunov@gmail.com>
2016-01-15 07:45:00 +0100

    put the graphics together

--- algoritem_testiranja_LS_EN.R
+++ algoritem_testiranja_LS_EN.R
@@ -1,14 +1,101 @@
 # iščemo funkcionalno redundanco v ekosistemu (functional redundancy) -
 
+library(xlsx)
+library(ggplot2)
+library(gridExtra)
+source("functions.R")
+
+# uvozimo podatke
+spe <- read.xlsx("./data/data_20142005.xlsx", sheetName = "morphospace1")
+coms <- read.xlsx("./data/data_20142005.xlsx", sheetName = "communities", encoding = "UTF-8")
+
+# naredimo clustering samo s telesno dolžino in gnatopodi
+spe.hc <- hclust(dist(spe[, grepl("gpI|log\\.body", names(spe))]), method = "ward.D2")
+plot(spe.hc, labels = spe$Species)
+
 # 1. najprej naredimo clustering in režemo tako, da dobimo dve skupini
+spe$group <- cutree(spe.hc, k = 2)
+
+# subset of only key variables
+spe <- spe[, grepl("log\\.bodyLenght|gp|Species|ecomorph|group", names(spe))]
+
 # 2. naredimo nulti model razlike med klastri (guildi) iz vseh vrst tako, da iz naključnih parov izračunamo
 #    evklidske razdalje -> histogram
+
+# find how many unique pairs can be generated
+# all possible combinations
+# all.combos <- combn(x = 1:nrow(spe), m = 2)
+# contrained between cluster pairs
+all.combos <- t(expand.grid(predator = 1:10, microf = 11:18))
+
+# for each pair, calculate distance
+null.model.diff.clust <- apply(all.combos, MARGIN = 2, function(x, d) {
+    euclidDistance(
+        d[sample(1:nrow(d), size = 2, replace = FALSE), ]
+    )
+}, d = spe[, !grepl("Species|ecomorph", names(spe))])
+
+# draw histogram of counts/density
+ggplot(data.frame(x = null.model.diff.clust), aes(x = x)) +
+    theme_bw() +
+    geom_histogram()
+
 # 3. za vsako združbo (8) izračunamo razliko med centroidoma klastrov (zelen x),
 #    ta delta X narišeš (izračunaš p) na histogram. pričakujemo, da bo na zgornjem koncu histograma
 #    to ponovimo za vse združbe (8x)
-#
+diff.btw.clust <- sapply(levels(coms$Locality), FUN = actualDiffClusters, coms = coms)
+
+
+diff.btw.clust <- data.frame(distance = diff.btw.clust, community = names(diff.btw.clust))
+rownames(diff.btw.clust) <- NULL
+
+#  calculate p-value
+diff.btw.clust$p.val <- sapply(diff.btw.clust$distance, FUN = function(x) prop(table(x < null.model.diff.clust)))
+diff.btw.clust$label <- sprintf("%s: %f", diff.btw.clust$community, round(diff.btw.clust$p.val, 3))
+diff.btw.clust
+
+last_plot() +
+    geom_vline(data = diff.btw.clust, aes(xintercept = distance)) +
+    geom_text(data = diff.btw.clust, aes(x = distance, y = 1 + c(12, 12, 10, 12, 12, 13, 7.5, 12), label = label),
+              angle = 90, vjust = 1, size = 3)
+
+t.test(x = null.model.diff.clust, y = diff.btw.clust$distance)
+var.test(x = null.model.diff.clust, y = diff.btw.clust$distance)
+
+sd(diff.btw.clust$distance)
+sd(null.model.diff.clust)
+
 # 4. za vsak klastr:
-#    preštejemo število vrst v klastru in glede na število (2, 3) naredimo nulti model - izbiramo 2 ali 3 vrste
+#    preštejemo število vrst iz klastru v združbi in glede na število (2, 3) naredimo nulti model - izbiramo 2 ali 3 vrste
 #    iz vseh vrst glavnega drevesa
+null.model.2 <- apply(X = combn(x = 1:nrow(spe), m = 2), MARGIN = 2, FUN = nullCluster2, spe = spe)
+
+ggplot(data.frame(x = null.model.2), aes(x = x)) +
+    theme_bw() +
+    geom_histogram()
+
+# tole nariši na nulti model skupaj s p-ji
+ng.nm <- euclidDistance(spe[spe$Species %in% c("N_grandii", "N_microcerberus"), !grepl("Species|ecomorph|group", x = names(spe))])
+nl.np <- euclidDistance(spe[spe$Species %in% c("N_longidactylus", "N_pupetta"), !grepl("Species|ecomorph|group", x = names(spe))])
+c.nt <- euclidDistance(spe[spe$Species %in% c("Carinurella", "N_transitivus"), !grepl("Species|ecomorph|group", x = names(spe))])
+nk.nl <- euclidDistance(spe[spe$Species %in% c("N_kochianus", "N_longidactylus"), !grepl("Species|ecomorph|group", x = names(spe))])
+nm.ns <- euclidDistance(spe[spe$Species %in% c("N_multipennatus", "N_serbicus"), !grepl("Species|ecomorph|group", x = names(spe))])
+na.nf <- euclidDistance(spe[spe$Species %in% c("N_aquilex", "N_fontanus"), !grepl("Species|ecomorph|group", x = names(spe))])
+
+t.test(x = null.model.2, y = c(ng.nm, nl.np, c.nt, nk.nl, nm.ns, na.nf))
+
 # 5. izračunamo minimum spanning tree (MST) iz evklidskih razdalj oz. evklidsko
 #    razdaljo za dve vrsti, in to narišemo na histogram oz. izračunamo p (10 grafov)
+
+null.model.3 <- apply(X = combn(x = 1:nrow(spe), m = 3), MARGIN = 2, FUN = nullCluster3, spe = spe)
+
+ggplot(data.frame(x = null.model.3), aes(x = x)) +
+    theme_bw() +
+    geom_histogram()
+
+# tole nariši na nulti model skupaj s p-ji
+kmp <- mst(spe[spe$Species %in% c("N_kenki_5", "N_multipennatus", "N_pectinicauda"), !grepl("Species|ecomorph|group", x = names(spe))])
+pll <- mst(spe[spe$Species %in% c("N_pupetta", "N_labacensis", "N_longidactylus"), !grepl("Species|ecomorph|group", x = names(spe))])
+gmd <- mst(spe[spe$Species %in% c("N_grandii", "N_microcerberus", "N_dolenianesis"), !grepl("Species|ecomorph|group", x = names(spe))])
+
+t.test(x = null.model.3, y = c(kmp, pll, gmd))
--- functions.R
+++ functions.R
@@ -1,6 +1,7 @@
 ## ---- functions ----
 
 # Function to calculate ratio between TRUE/FALSE
+# x = boolean vector
 prop <- function(x) {
    dolz <- sum(x)
    x["TRUE"]/dolz
@@ -17,32 +18,32 @@ euclidDistance <- function(x) {
 # parameters of actual species.
 
 calculateEuclideanForTwoSpecies <- function(input, MLP = 2, desc, N = 10000) {
-   # input = raw data, where one column is species name and the rest are morphological 
+   # input = raw data, where one column is species name and the rest are morphological
    #         characters (already transformed, e.g. log of body size)
    # MLP = to get 10000 simulations, multiply by this constant
-   # desc = description of species, one column is location, one is species name and 
+   # desc = description of species, one column is location, one is species name and
    # other columns are ignored
    # N = number of simulated (pair-wise) values
-   
+
    # calculate range of variable (character)
-   character.range <- sapply(input[, -which(names(input) == "species")], range) 
-   
+   character.range <- sapply(input[, -which(names(input) == "species")], range)
+
    # simulate N *MLP virtual species with 17 characters each
-   vir.spec.two <- t(replicate(N * MLP, apply(character.range, MARGIN = 2, 
+   vir.spec.two <- t(replicate(N * MLP, apply(character.range, MARGIN = 2,
             FUN = function(x) runif(n = 1, min = x[1], max = x[2]))))
-   
+
    # construct a vector that will enable us to compare two and two species
-   split.along <- rep(1:((N * MLP)/2), each = 2) 
-   
-   sim.euc <- data.frame(vals = unclass(by(vir.spec.two, INDICES = split.along, 
+   split.along <- rep(1:((N * MLP)/2), each = 2)
+
+   sim.euc <- data.frame(vals = unclass(by(vir.spec.two, INDICES = split.along,
             FUN = function(x) sqrt(sum(sapply(x, diff)^2)))))
-   
+
    # Find which localities have two species in a community.
    twos <- rle(as.character(desc$locality))$lengths == 2
    twos <- droplevels(unique(desc$locality)[twos])
    twos <- droplevels(desc[desc$locality %in% twos, ])
    twos <- split(twos, f = twos$locality)
-   
+
    # Calculate Euclidean distance of real pairs.
    real.ed <- unlist(lapply(twos, FUN = function(x, data) {
             cte <- data[data$species %in% x$species, ]
@@ -50,20 +51,20 @@ calculateEuclideanForTwoSpecies <- function(input, MLP = 2, desc, N = 10000) {
             names(ed) <- paste(cte$species, collapse = " - ")
             ed
          }, data = input))
-   
+
    # Find what proportion of simulated values are bigger than values from real pairs.
    sim.bigger.real <- sapply(real.ed, FUN = function(x, sim.vals) {
          rslt <- prop(table(sim.vals > x))
          names(rslt) <- names(x)
          rslt
       }, sim.vals = unname(sim.euc$vals))
-   
+
    # We round the numbers
    real.ed <- data.frame(vals = real.ed)
-   
+
    lbls <- sapply(twos, FUN = function(x) sapply(x$species, FUN = gsub, pattern = "_", replacement = ". "))
    lbls <- apply(lbls, MARGIN = 2, FUN = function(x) paste(x, collapse = " - "))
-   
+
    output <- list(sim.euc = sim.euc,
       real.euc = real.ed,
       pval = sim.bigger.real,
@@ -75,25 +76,25 @@ calculateEuclideanForTwoSpecies <- function(input, MLP = 2, desc, N = 10000) {
 # is constructed as in calculateEuclideanForTwoSpecies().
 
 calculateSDforThreeSpecies <- function(input, MLP, desc, N = 10000) {
-   # input = raw data, where one column is species name and the rest are morphological 
+   # input = raw data, where one column is species name and the rest are morphological
    # characters (already transformed, e.g. log of body size)
    # MLP = to get 10000 simulations, multiply by this constant
    # desc = description of species, one column is location, one is species name and
    # other columns are ignored
-   
+
    # calculate range of variable (character)
-   character.range <- sapply(input[, -which(names(input) == "species")], range) 
-   
-   vir.spec.three <- t(replicate(N * MLP, apply(character.range, MARGIN = 2, 
+   character.range <- sapply(input[, -which(names(input) == "species")], range)
+
+   vir.spec.three <- t(replicate(N * MLP, apply(character.range, MARGIN = 2,
             FUN = function(x) runif(n = 1, min = x[1], max = x[2]))))
    split.along <- rep(1:((N * MLP)/3), each = 3)
-   
+
    # Find which localities have three species in a community.
    threes <- rle(as.character(desc$locality))$lengths == 3
    threes <- droplevels(unique(desc$locality)[threes])
    threes <- droplevels(desc[desc$locality %in% threes, ])
    threes <- split(threes, f = threes$locality)
-   
+
    # Function that calculates standard deviation of a triplet.
    calculateSD <- function(x) {
       combos <- matrix(c(1,2, 1,3, 2, 3), byrow = TRUE, ncol = 2)
@@ -102,27 +103,27 @@ calculateSDforThreeSpecies <- function(input, MLP, desc, N = 10000) {
       top3 <- which(get.sd == max(get.sd))
       sd(get.sd[-top3]) # calculate SD based on two shortest euclidean distances
    }
-   
+
    # Calculate standard deviation from all combinations of euclidean distances
    # of three species in a community.
    sim.sd <- data.frame(vals = unclass(by(vir.spec.three, INDICES = split.along, FUN = calculateSD)))
-   
+
    # Calculate sd of euclidean distances for real communities.
    real.sd <- data.frame(vals = unlist(lapply(threes, FUN = function(m, data) {
                to.clclt <- data[data$species %in% m$species, ]
                calculateSD(to.clclt[, -which(names(to.clclt) == "species")])
             }, data = input)))
-   
+
    # Find what proportion of simulated values are bigger compared to real.
    sim.bigger.real <- apply(real.sd, MARGIN = 1, FUN = function(x, sim.vals) {
          rslt <- prop(table(sim.vals > x))
          names(rslt) <- names(x)
          rslt
       }, sim.vals = sim.sd$vals)
-   
+
    lbls <- sapply(threes, FUN = function(x) sapply(x$species, FUN = gsub, pattern = "_", replacement = ". "))
    lbls <- apply(lbls, MARGIN = 2, FUN = function(x) paste(x, collapse = " - "))
-   
+
    output <- list(sim.euc = sim.sd,
       real.euc = real.sd,
       pval = sim.bigger.real,
@@ -133,14 +134,14 @@ calculateSDforThreeSpecies <- function(input, MLP, desc, N = 10000) {
 drawFigure <- function(x, xlab = c("sd", "ed"), label = TRUE) {
    # x = object from a simulation function (calculateEuclideanForTwoSpecies
    # or calculateSDforThreeSpecies)
-   # xlab = what to put as label for x axis 
-   
+   # xlab = what to put as label for x axis
+
    if (xlab == "sd") {
       xlab <- "St. dev. of euclidean distances between virtual species"
    } else {
       xlab <- "Euclidean distances between virtual species"
    }
-   
+
    gg.object <- ggplot(x[["sim.euc"]], aes(x = vals)) +
       theme_bw() +
       xlab(xlab) +
@@ -148,7 +149,7 @@ drawFigure <- function(x, xlab = c("sd", "ed"), label = TRUE) {
       ylab("Frequency") +
       geom_histogram(fill = "light gray") +
       geom_vline(xintercept = x[["real.euc"]]$vals, size = 0.8)
-   
+
    if (label) {
       # Calulate relative position of labels, thanks to Paul
       # http://stackoverflow.com/questions/7705345/how-can-i-extract-plot-axes-ranges-for-a-ggplot2-object
@@ -157,13 +158,51 @@ drawFigure <- function(x, xlab = c("sd", "ed"), label = TRUE) {
       position.l <- position.l - 0.5 * position.l
       # and construct labels
       lbls <- paste(x[["pval"]], x[["labels"]], sep = "    ")
-      
-      gg.object + 
-         geom_text(data = x[["real.euc"]], aes(x = vals), y = position.l, label = lbls, angle = -90, 
+
+      gg.object +
+         geom_text(data = x[["real.euc"]], aes(x = vals), y = position.l, label = lbls, angle = -90,
             vjust = -0.7, size = 3)
    }
    gg.object
-   
+
    # preberi koliko ima vrst združba
-   # naredi test 
+   # naredi test
 }
+
+#
+actualDiffClusters <- function(loc, coms) {
+    one.com <- droplevels(coms[coms$Locality == loc, "Species"])
+    one.com <- spe[spe$Species %in% one.com, ]
+
+    if (length(unique(one.com$group)) == 1) {
+        message("Only one group present at locality")
+        return(NA)
+    }
+
+    # for each group, find cluster mean...
+    clust.means <- aggregate(. ~ group,
+                             FUN = mean, na.rm = TRUE,
+                             data = one.com[, !grepl("Species|ecomorph", names(one.com))])
+
+    # ... and find euclidean difference between them
+    clust.group.diff <- euclidDistance(clust.means[, !grepl("group", names(clust.means))])
+    return(clust.group.diff)
+}
+# Calculate null distribution of a cluster given the number of species.
+nullCluster2 <- function(x, spe) {
+    # browser()
+    ss <- spe[x, ]
+    euclidDistance(ss[, !grepl("group|ecomorph|Species", names(ss))])
+}
+
+nullCluster3 <- function(x, spe) {
+    mst(spe[x, !grepl("Species|ecomorph|gro", x = names(spe))])
+}
+
+mst <- function(x) {
+    combos <- combn(3, 2)
+    euc <- apply(combos, MARGIN = 2, FUN = function(m, x) {
+        euclidDistance(x[m, ])
+    }, x = x)
+    sum(sort(euc)[1:2])
+    }
